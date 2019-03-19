82435785ed222d7650f15af1a5805ec861c2e94d
Kevin <kevinyangyuhao@gmail.com>
2019-03-19 13:22:16 -0400

    ai project 2 first draft
    initial

---
+++ AI Project 2/src/gameTreeNode.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class gameTreeNode {
+	gameTreeNode root;
+	List<gameTreeNode> children;
+	gameTreeNode parent;
+	int value;
+	
+	public gameTreeNode
+}
---
+++ AI Project 2/src/tictactoe.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class tictactoe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 3;
+		char[] ch = new char[4];
+		char cha = '\u0000';
+		boolean b = (ch[0] == cha);
+		System.out.println(b);
+		
+	}
+
+	public static void helper(int x) {
+		x = 4;
+		helper2(x);
+	}
+	
+	public static void helper2(int m) {
+		System.out.println(m);
+	}
+}
